18c03a2ea06941ebf44c797a106a25cd8bd55ad2
Mateo Sierra <m.sierra1037@hotmail.com>
2021-06-03 16:28:07 -0500

    js navbar file correction

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -5,15 +5,16 @@ let open = false;
 hamburgerButton.addEventListener("click", () => {
     if (!open) {
         hamburgerButton.style.color = "#fff"
-        hamburgerButton.style.margin = "0em 0px 0px 88vw"
+        hamburgerButton.style.margin = "-0.25em 0px 0px 86vw"
         hamburgerButton.style.transform = "rotate(45deg)"
+        hamburgerButton.style.fontSize = "60px";
         hamburgerButton.innerHTML = "+"
-
     } else {
         hamburgerButton.style.color = "#000"
         hamburgerButton.innerHTML = "|||"
         hamburgerButton.style.margin = "0em 0px 0px 85vw"
         hamburgerButton.style.transform = "rotate(90deg)"
+        hamburgerButton.style.fontSize = "40px";
     }
     open = !open;
 });
